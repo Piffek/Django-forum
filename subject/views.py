c02fe4996a02ce6cf3fb91a6220b58f1a7aa7379
Piffek <patrykpiwko123412@gmail.com>
2017-03-07 17:03:57 +0100

    add get_context_data in subject view

--- subject/views.py
+++ subject/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, TemplateView
 from .models import Subject
+from posts.models import Posts
 from django.http import HttpResponse
 from django.template.context_processors import request
 from pip._vendor.requests.models import Request
@@ -10,5 +11,10 @@ class SubjectListView(ListView):
     
 class SubjectDetailView(DetailView):
     model = Subject
+    
+    def get_context_data(self, **kwargs):
+        context = super(SubjectDetailView, self).get_context_data(**kwargs)
+        context['posts_obj'] = Posts.objects.filter(subj=self.kwargs['pk']).order_by('-when')
+        return context
 
 
