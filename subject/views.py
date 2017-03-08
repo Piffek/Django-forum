767069bc5a8373b1ae9776e7266abadc4d22abe8
Piffek <patrykpiwko123412@gmail.com>
2017-03-08 14:13:49 +0100

    function add subject

--- subject/views.py
+++ subject/views.py
@@ -5,18 +5,30 @@ from posts.models import Posts
 from django.http import HttpResponse
 from django.template.context_processors import request
 from pip._vendor.requests.models import Request
+from .forms import SubjectForm
+from django.utils.timezone import now
 
 class SubjectListView(ListView):
     model = Subject
     
-    
 class SubjectDetailView(DetailView):
     model = Subject
-
+    
     def get_context_data(self, **kwargs):
-        context = super(SubjectDetailView, self).get_context_data(**kwargs)
-        context['posts_obj'] = Posts.objects.filter(subj = self.kwargs['pk']).order_by('-when')
+        context = super().get_context_data(**kwargs)
+        context['posts_obj'] = Posts.objects.filter(subj=self.kwargs['pk']).order_by('-when')
         return context
+    
+def subjectNew(request):
+    if request.method == "POST":
+        form = SubjectForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.who = request.user
+            post.save()
+    else:
+        form = SubjectForm()
+    return render(request, 'subject/new_subject.html', {'form': form})
 
 
 
